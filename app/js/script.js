418afd4c7415248f5809f05120833b21208caa70
Gustavo Alves <galves@tractionguest.com>
2019-04-26 11:13:51 -0700

    First Commit

---
+++ app/js/script.js
@@ -0,0 +1,172 @@
+var element = document.getElementById('grid-snap'),
+    x = 0, y = 0;
+var dropzone = document.getElementById('dropzone');
+
+interact(".draggable[data-eltype*='object").on('tap', editStyle)
+  .draggable({
+    modifiers: [
+      interact.modifiers.snap({
+        targets: [
+          interact.createSnapGrid({ x: 30, y: 30 })
+        ],
+        range: Infinity,
+        relativePoints: [ { x: 0, y: 0 } ]
+      }),
+      interact.modifiers.restrict({
+        restriction: dropzone,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+        endOnly: true
+      })
+    ],
+    onstart: dragStartEndListener,
+    onend: dragStartEndListener,
+    onmove: dragMoveListener
+  }).resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent',
+        endOnly: true,
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 },
+      }),
+    ],
+
+    inertia: true,
+    onmove: resizeMoveListener
+  });
+
+  interact(".draggable[data-eltype*='text")
+  .draggable({
+    modifiers: [
+      interact.modifiers.snap({
+        targets: [
+          interact.createSnapGrid({ x: 30, y: 30 })
+        ],
+        range: Infinity,
+        relativePoints: [ { x: 0, y: 0 } ]
+      }),
+      interact.modifiers.restrict({
+        restriction: dropzone,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+        endOnly: true
+      })
+    ],
+    onstart: dragStartEndListener,
+    onend: dragStartEndListener,
+    onmove: dragMoveListener
+  }).resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: false, top: false },
+
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent',
+        endOnly: true,
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 },
+      }),
+    ],
+
+    inertia: true,
+    onmove: resizeMoveListener
+  });
+
+  interact('#dropzone').dropzone({
+    // only accept elements matching this CSS selector
+    accept: '.draggable',
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.1,
+  
+    // listen for drop related events:
+  
+    ondropactivate: function (event) {
+      // add active dropzone feedback
+      event.target.classList.add('drop-active')
+    },
+    ondragenter: function (event) {
+      var draggableElement = event.relatedTarget;
+      var dropzoneElement = event.target;
+  
+      // feedback the possibility of a drop
+      dropzoneElement.classList.add('drop-target')
+      draggableElement.classList.add('can-drop')
+      //draggableElement.textContent = 'Dragged in'
+    },
+    ondragleave: function (event) {
+      // remove the drop feedback style
+      event.target.classList.remove('drop-target')
+      event.relatedTarget.classList.remove('can-drop')
+      //event.relatedTarget.textContent = 'Dragged out'
+    },
+    ondrop: function (event) {
+      //event.relatedTarget.textContent = 'Dropped'
+    },
+    ondropdeactivate: function (event) {
+      // remove active dropzone feedback
+      event.target.classList.remove('drop-active')
+      event.target.classList.remove('drop-target')
+    }
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  function dragStartEndListener(event){
+    var target = event.target;
+    target.classList.toggle('hold');
+  }
+
+  function editStyle(event){
+    var target = event.target;
+    target.style.background = "red";
+  }
+
+  function resizeMoveListener(event){
+    var target = event.target,
+    x = (parseFloat(target.getAttribute('data-x')) || 0),
+    y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    //target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+    };
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+  window.resizeMoveListener = resizeMoveListener;
+  window.dragStartEndListener = dragStartEndListener;
